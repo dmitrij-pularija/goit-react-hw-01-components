5ff03ff2c0f9940a4f77cd2ae50d94278d9bfb88
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-01-15 12:13:00 +0200

    bags fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import 'modern-normalize';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter basename='/goit-react-hw-01-components'>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>
+// );
+
+ReactDOM.render(
   <StrictMode>
     <BrowserRouter basename='/goit-react-hw-01-components'>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById('root'),
 );
